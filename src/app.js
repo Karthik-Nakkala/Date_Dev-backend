b51e2078f922cf6ee4202c92277a8bbd2442b54e
Karthik Yadav <karthinakkala15@gmail.com>
2026-07-02 18:23:11 +0530

    add Multiple route handlers to same request

--- src/app.js
+++ src/app.js
@@ -2,28 +2,29 @@ const express=require("express");
 
 const app=express();
 
-app.get('/user',(req,res)=>{
-    res.send({name:"Karthik",role:"Fullstack Developer"});
-});
-
-app.post("/user",(req,res)=>{
-    res.send("User had been posting");
-})
-
-app.put("/user",(req,res)=>{
-    res.send("User is updating entire document!");
-})
-
-app.patch("/user",(req,res)=>{
-    res.send("User is updating some of fields in document");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("User is deleting data");
-})
+app.get('/rebelhood',(req,res,next)=>{
+    next();
+    console.log("They are none other than, The great Jakkanna and The Dinosaur/Karna/RaguNandhana/Mr.Perfect/Darling");
+    res.send({
+        name:"Praboss",
+        profession:"Being in Hearts for millions",
+    });
+},[(req,res,next)=>{
+    console.log("Every face He wear becomes his real face. The world doesn't remember him, The world remembers who he pretended to be");
+    next();
+},(req,res,next)=>{
+    console.log("Baahubali is a war for him");
+    next();
+},(req,res,next)=>{
+    console.log("But, He is a dinosaur, no war stops him");
+    next();
+}],(req,res,next)=>{
+    console.log("From Bahubali, Two Men are still never looking back!, Guess Who are them?");
+    next();
+},)
 
 
 
 app.listen(7777,()=>{
-    console.log("Thala for a reason");
+    console.log("Whole India, Only one hood is there, That is Rebelhood!");
 });
